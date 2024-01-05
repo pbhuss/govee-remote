ba97e74a6e1b6081ef17284152b8dc25bea54647
Peter Huss <pbhuss@users.noreply.github.com>
2024-01-04 22:29:32 -0800

    Refactor repetitive code

--- govee_remote/gui.py
+++ govee_remote/gui.py
@@ -16,6 +16,7 @@ from govee_remote.client import GoveeClient
 from govee_remote.color import get_color
 from govee_remote.color import get_luma
 from govee_remote.color import KELVIN_RGB
+from govee_remote.color import RGB
 
 
 STATE_FILE = "data/state.yaml"
@@ -35,13 +36,9 @@ class State:
     @classmethod
     def load(cls) -> State:
         if not os.path.exists(STATE_FILE):
-            return cls.default_state()
+            return cls()
         with open(STATE_FILE) as fp:
             return State(**yaml.safe_load(fp)["state"])
-
-    @classmethod
-    def default_state(cls) -> State:
-        return cls(color="kelvin", brightness=100, on=True, kelvin=3000)
 
 
 class ButtonMap:
@@ -57,6 +54,62 @@ class ButtonMap:
                 yield key
 
 
+class UIConfig:
+    def __init__(self) -> None:
+        self.button_width = 60
+        self.button_height = 30
+        self.large_button_width = 90
+        self.large_button_height = 45
+        self.cell_width = 190
+        self.cell_height = 40
+        self.left_padding = 20
+        self.top_padding = 100
+        self.font = pygame.font.SysFont("Arial", 14)
+        self.font_large = pygame.font.SysFont("Arial", 22, bold=1)
+
+
+def add_button(
+    conf: UIConfig,
+    button_map: ButtonMap,
+    color: RGB | str,
+    row: int,
+    col: int,
+    label: str,
+    name: str,
+    screen: pygame.Surface,
+) -> pygame.Rect:
+    rect = pygame.Rect(
+        conf.left_padding + col * conf.cell_width,
+        conf.top_padding + row * conf.cell_height,
+        conf.button_width,
+        conf.button_height,
+    )
+    pygame.draw.rect(screen, color, rect)
+    pygame.draw.rect(screen, "black", rect, width=2)
+    color_name = conf.font.render(label, 1, "black")
+    screen.blit(
+        color_name,
+        color_name.get_rect(left=rect.right + 10, centery=rect.centery),
+    )
+    button_map.register(
+        name,
+        pygame.Rect(
+            conf.left_padding + col * conf.cell_width,
+            conf.top_padding + row * conf.cell_height,
+            conf.cell_width - 10,
+            conf.button_height,
+        ),
+    )
+    return rect
+
+
+def add_checkmark(
+    conf: UIConfig, screen: pygame.Surface, rect: pygame.Rect, color: RGB | str
+) -> None:
+    checkmark = conf.font_large.render("X", 1, color)
+    screen.blit(checkmark, checkmark.get_rect(center=rect.center))
+
+
 def redraw(screen: pygame.Surface, state: State) -> ButtonMap:
     button_map = ButtonMap()
 
@@ -67,153 +120,120 @@ def redraw(screen: pygame.Surface, state: State) -> ButtonMap:
         key=lambda c: tuple(rgb_to_hsv(to_rgb(c))),
     )
 
-    font = pygame.font.SysFont("Arial", 14)
-    font_large = pygame.font.SysFont("Arial", 22, bold=1)
-
-    button_width = 60
-    button_height = 30
-    large_button_width = 90
-    large_button_height = 45
-    cell_width = 190
-    cell_height = 40
-    left_padding = 20
-    top_padding = 100
+    conf = UIConfig()
 
     on_color = "yellow" if state.on else "lightgray"
-    on_rect = pygame.Rect(left_padding, 30, large_button_width, large_button_height)
+    on_rect = pygame.Rect(
+        conf.left_padding, 30, conf.large_button_width, conf.large_button_height
+    )
     button_map.register("power.on", on_rect)
     pygame.draw.rect(screen, get_color(on_color), on_rect)
     pygame.draw.rect(screen, "black", on_rect, width=2)
-    on_text = font_large.render("On", True, "black")
+    on_text = conf.font_large.render("On", True, "black")
     screen.blit(on_text, on_text.get_rect(center=on_rect.center))
 
     off_color = "lightgray" if state.on else "yellow"
     off_rect = pygame.Rect(
-        left_padding + large_button_width + 20,
+        conf.left_padding + conf.large_button_width + 20,
         30,
-        large_button_width,
-        large_button_height,
+        conf.large_button_width,
+        conf.large_button_height,
     )
     button_map.register("power.off", off_rect)
     pygame.draw.rect(screen, get_color(off_color), off_rect)
     pygame.draw.rect(screen, "black", off_rect, width=2)
-    off_text = font_large.render("Off", True, "black")
+    off_text = conf.font_large.render("Off", True, "black")
     screen.blit(off_text, off_text.get_rect(center=off_rect.center))
 
     for col, batch in enumerate(batched(names, 20)):
         for row, name in enumerate(batch):
-            rect = pygame.Rect(
-                left_padding + col * cell_width,
-                top_padding + row * cell_height,
-                button_width,
-                button_height,
-            )
-            pygame.draw.rect(screen, get_color(name), rect)
-            pygame.draw.rect(screen, "black", rect, width=2)
-            color_name = font.render(name.capitalize(), 1, "black")
-            screen.blit(
-                color_name,
-                color_name.get_rect(left=rect.right + 10, centery=rect.centery),
-            )
-            button_map.register(
-                f"color.{name}",
-                pygame.Rect(
-                    left_padding + col * cell_width,
-                    top_padding + row * cell_height,
-                    cell_width - 10,
-                    button_height,
-                ),
+            color = get_color(name)
+            rect = add_button(
+                conf=conf,
+                button_map=button_map,
+                color=color,
+                row=row,
+                col=col,
+                label=name.capitalize(),
+                name=f"color.{name}",
+                screen=screen,
             )
             if name == state.color:
-                color = "black" if get_luma(get_color(name)) > 130 else "white"
-                checkmark = font_large.render("X", 1, color)
-                screen.blit(checkmark, checkmark.get_rect(center=rect.center))
+                add_checkmark(
+                    conf=conf,
+                    screen=screen,
+                    rect=rect,
+                    color="black" if get_luma(color) > 130 else "white",
+                )
 
     row += 1
 
-    brightness_label = font_large.render("Brightness", 1, "black")
+    brightness_label = conf.font_large.render("Brightness", 1, "black")
     screen.blit(
         brightness_label,
-        (left_padding + col * cell_width, top_padding + row * cell_height + 5),
+        (
+            conf.left_padding + col * conf.cell_width,
+            conf.top_padding + row * conf.cell_height + 5,
+        ),
     )
 
     brightnesses = [1, *range(10, 101, 10)]
     brightnesses.reverse()
     for row, brightness in enumerate(brightnesses, start=row + 1):
-        rect = pygame.Rect(
-            left_padding + col * cell_width,
-            top_padding + row * cell_height,
-            button_width,
-            button_height,
-        )
-        pygame.draw.rect(
-            screen, tuple(int(255 * brightness / 100) for _ in range(3)), rect
-        )
-        pygame.draw.rect(screen, "black", rect, width=2)
-        brightness_name = font.render(f"{brightness}%", 1, "black")
-        screen.blit(
-            brightness_name,
-            brightness_name.get_rect(left=rect.right + 10, centery=rect.centery),
-        )
-        button_map.register(
-            f"brightness.{brightness}",
-            pygame.Rect(
-                left_padding + col * cell_width,
-                top_padding + row * cell_height,
-                cell_width - 10,
-                button_height,
-            ),
+        c = int(255 * brightness / 100)
+        rect = add_button(
+            conf=conf,
+            button_map=button_map,
+            color=(c, c, c),
+            row=row,
+            col=col,
+            label=f"{brightness}%",
+            name=f"brightness.{brightness}",
+            screen=screen,
         )
         if brightness == state.brightness:
-            color = "black" if brightness >= 50 else "white"
-            checkmark = font_large.render("X", 1, color)
-            screen.blit(checkmark, checkmark.get_rect(center=rect.center))
+            add_checkmark(
+                conf=conf,
+                screen=screen,
+                rect=rect,
+                color="black" if brightness >= 50 else "white",
+            )
 
     row += 1
 
-    kelvin_label = font_large.render("Illumination Mode", 1, "black")
+    kelvin_label = conf.font_large.render("Illumination Mode", 1, "black")
     screen.blit(
         kelvin_label,
-        (left_padding + col * cell_width, top_padding + row * cell_height + 5),
+        (
+            conf.left_padding + col * conf.cell_width,
+            conf.top_padding + row * conf.cell_height + 5,
+        ),
     )
 
     row += 1
 
-    rect = pygame.Rect(
-        left_padding + col * cell_width,
-        top_padding + row * cell_height,
-        button_width,
-        button_height,
-    )
-    pygame.draw.rect(screen, KELVIN_RGB[state.kelvin], rect)
-    pygame.draw.rect(screen, "black", rect, width=2)
-    brightness_name = font.render(f"{state.kelvin}K", 1, "black")
-    screen.blit(
-        brightness_name,
-        brightness_name.get_rect(left=rect.right + 10, centery=rect.centery),
-    )
-    button_map.register(
-        "color.kelvin",
-        pygame.Rect(
-            left_padding + col * cell_width,
-            top_padding + row * cell_height,
-            cell_width - 30,
-            button_height,
-        ),
+    rect = add_button(
+        conf=conf,
+        button_map=button_map,
+        color=KELVIN_RGB[state.kelvin],
+        row=row,
+        col=col,
+        label=f"{state.kelvin}K",
+        name="color.kelvin",
+        screen=screen,
     )
     if state.color == "kelvin":
-        checkmark = font_large.render("X", 1, "black")
-        screen.blit(checkmark, checkmark.get_rect(center=rect.center))
+        add_checkmark(conf=conf, screen=screen, rect=rect, color="black")
 
     kelvin_plus = pygame.Rect(
         rect.right + 100,
-        top_padding + row * cell_height,
-        button_height,
-        button_height,
+        conf.top_padding + row * conf.cell_height,
+        conf.button_height,
+        conf.button_height,
     )
     pygame.draw.rect(screen, "lightblue", kelvin_plus)
     pygame.draw.rect(screen, "black", kelvin_plus, width=2)
-    plus_text = font.render("+", 1, "black")
+    plus_text = conf.font.render("+", 1, "black")
     screen.blit(
         plus_text,
         plus_text.get_rect(center=kelvin_plus.center),
@@ -222,13 +242,13 @@ def redraw(screen: pygame.Surface, state: State) -> ButtonMap:
 
     kelvin_minus = pygame.Rect(
         kelvin_plus.right + 10,
-        top_padding + row * cell_height,
-        button_height,
-        button_height,
+        conf.top_padding + row * conf.cell_height,
+        conf.button_height,
+        conf.button_height,
     )
     pygame.draw.rect(screen, "red", kelvin_minus)
     pygame.draw.rect(screen, "black", kelvin_minus, width=2)
-    minus_text = font.render("-", 1, "black")
+    minus_text = conf.font.render("-", 1, "black")
     screen.blit(
         minus_text,
         minus_text.get_rect(center=kelvin_minus.center),
@@ -279,21 +299,12 @@ def handle_click(
             redraw(screen, state)
 
 
-def main(client: GoveeClient) -> None:
-    pygame.init()
-    pygame.display.set_caption("Govee Remote")
-    client.on()
-    state = State.load()
-    if state.color == "kelvin":
-        client.color_kelvin(state.kelvin)
-    else:
-        client.color(state.color)
-    client.brightness(state.brightness)
-    screen = pygame.display.set_mode((1600, 900))
+def loop(
+    state: State, client: GoveeClient, screen: pygame.Surface, button_map: ButtonMap
+) -> None:
+    hits = set()
     clock = pygame.time.Clock()
     running = True
-    button_map = redraw(screen, state)
-    hits = set()
     while running:
         for event in pygame.event.get():
             match event.type:
@@ -310,6 +321,21 @@ def main(client: GoveeClient) -> None:
                                 name=name, state=state, client=client, screen=screen
                             )
         clock.tick(30)
+
+
+def main(client: GoveeClient) -> None:
+    pygame.init()
+    pygame.display.set_caption("Govee Remote")
+    client.on()
+    state = State.load()
+    if state.color == "kelvin":
+        client.color_kelvin(state.kelvin)
+    else:
+        client.color(state.color)
+    client.brightness(state.brightness)
+    screen = pygame.display.set_mode((1600, 900))
+    button_map = redraw(screen, state)
+    loop(state=state, client=client, screen=screen, button_map=button_map)
     state.save()
     client.off()
     pygame.quit()
